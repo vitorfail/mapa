71d522b042f1021f42ed1508481312d0c7166eab
vitorfail <vitor_andrademanoel@hotmail.com>
2022-10-06 15:13:36 -0300

    inicial

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import { Map, Marker} from 'react-map-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+const MAPBOX_TOKEN = '';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='descri'>
+
+      </div>
+      <div className='Map'>
+        <Map
+          initialViewState={{
+            latitude: 37.8,
+            longitude: -122.4,
+            zoom: 14
+          }}
+          style={{width: 800, height: 600}}
+          mapStyle="mapbox://styles/mapbox/streets-v9"
+          mapboxAccessToken={MAPBOX_TOKEN}>
+        <Marker longitude={-122.4} latitude={37.8} color="red" />
+      </Map>
+      </div>
+    </div>
+  );
+}
+export default App;
