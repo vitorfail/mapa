e966ec960930e7f05400d0b765957823a44485a1
vitorfail <vitor_andrademanoel@hotmail.com>
2022-10-06 23:56:36 -0300

    asdfdfdfd

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import './App.css';
-import { Map, Marker} from 'react-map-gl';
+import  ReactMapGL, {Marker} from 'react-map-gl'
 import 'mapbox-gl/dist/mapbox-gl.css';
+import Bola1 from './icons/bola1.png'
+import Bola2 from './icons/bola2.png'
+import Bola3 from './icons/bola3.png'
+import Bola4 from './icons/bola4.png'
 
 const MAPBOX_TOKEN = 'pk.eyJ1Ijoidml0b3JmYWlsIiwiYSI6ImNsOHhyajNjYjA4aXIzdW56ZDN1NjI0ZjUifQ.dYaH3rrTcs8WwFHCHCSCRQ';
+const dados = [[3, 38], [7, 39], [4, 38], [2, 40]]
+const imagens = [Bola1, Bola2, Bola3, Bola4]
+
 
 function App() {
   return (
@@ -11,17 +18,22 @@ function App() {
 
       </div>
       <div className='Map'>
-        <Map
+        <ReactMapGL
           initialViewState={{
-            latitude: 37.8,
-            longitude: -122.4,
-            zoom: 14
+            latitude: -3.71839,
+            longitude: -38.5434,
+            zoom: 6
           }}
-          style={{width: 800, height: 600}}
-          mapStyle="mapbox://styles/mapbox/streets-v9"
+
+          mapStyle="mapbox://styles/mapbox/dark-v9"
           mapboxAccessToken={MAPBOX_TOKEN}>
-        <Marker longitude={-122.4} latitude={37.8} color="red" />
-      </Map>
+            {dados.map((item, index) =>(
+                <Marker key={index} latitude={item[0]*-1} longitude={item[1]*-1} anchor="bottom" >
+                <div className='img'></div>
+              </Marker>
+
+            ))}
+      </ReactMapGL>
       </div>
     </div>
   );
